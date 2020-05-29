ecc71d7fe26f0bfd651f71319fa620a6c3608c01
Nikita Volobuev <nikitavbv@gmail.com>
2020-05-29 23:57:58 +0300

    feat(image): add google cloud sdk

--- script.sh
+++ script.sh
@@ -1,3 +1,5 @@
+source /google-cloud-sdk/path.bash.inc
+
 touch ~/.pgpass
 chmod 0600 ~/.pgpass
 echo $POSTGRES_HOST:${POSTGRES_PORT:-5432}:$POSTGRES_DATABASE:$POSTGRES_USER:$POSTGRES_PASSWORD > ~/.pgpass
